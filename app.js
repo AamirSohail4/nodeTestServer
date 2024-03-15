5fa94c0a62383c128740be2357554d722cb2b914
sakhawatullah063 <sakhawatullah063@outlook.com>
2024-03-15 10:29:23 +0500

    installing crose and bodyparser

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(cors());
+
 app.get("/api/home", (req, res) => {
   res.send("That is a Noode Server Response !");
+});
+app.get("/api/getRespons", (req, res) => {
+  res.send("Server Response Hello ");
 });
 
 app.listen(port, () => {
