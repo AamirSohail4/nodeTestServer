ea81baaed5c3a870af5b4c3eea2a957448b29df2
sakhawatullah063 <sakhawatullah063@outlook.com>
2024-03-15 12:16:55 +0500

    Changing the Port3000 to 8089

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 8080;
 
+// Middleware setup
 app.use(bodyParser.json());
 app.use(cors());
 
+// Define API routes
 app.get("/api/home", (req, res) => {
-  res.send("That is a Noode Server Response !");
-});
-app.get("/api/getRespons", (req, res) => {
-  res.send("Server Response Hello ");
+  res.send("This is a Node Server Response!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get("/api/getResponse", (req, res) => {
+  res.send("Server Response Hello");
+});
+
+// Start the server
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running and listening on port ${port}`);
 });
